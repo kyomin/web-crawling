c7c37d7bc962df8644528d9d8f31ec1b50a55991
kyomin <kim031504@naver.com>
2020-06-28 01:42:36 +0900

    userAgent를 이용해 한 탭으로 크롤링하여 봇이 아닌 사람이 작업하는 효과 주기

--- index.js
+++ index.js
@@ -1,57 +1,52 @@
 /* module import */
-const parse = require('csv-parse/lib/sync');
-const stringify = require('csv-stringify/lib/sync');
-const fs = require('fs');
+const xlsx = require('xlsx');
 const puppeteer = require('puppeteer');
+const add_to_sheet = require('./add_to_sheet');
 
 /* file import */
-const csv = fs.readFileSync('./csv/data.csv');
-const records = parse(csv.toString('utf-8'));
+const workbook = xlsx.readFile('./xlsx/data.xlsx');
+const ws = workbook.Sheets.영화목록;
+const records = xlsx.utils.sheet_to_json(ws);
 
 // 에러 처리 : 하나의 async 메소드 당 하나의 try ~ catch문을 넣어준다.
 const crawler = async () => {
     try {
-        const result = [];
         const browser = await puppeteer.launch({ headless: process.env.NODE.ENV === 'production' });
-        
-        // Promise.all에 의해 각 페이지를 동시에 처리를 할 것이다.
-        await Promise.all(records.map(async (record, idx) => {
-            try {
-                // 페이지 객체 생성
-                const page = await browser.newPage();
-                
-                // 해당 링크의 페이지 띄우기
-                await page.goto(record[1]);
-                
-                // 해당 클래스로 특정하여 태그를 찾는다.
-                const scoreEl = await page.$('.score.score_left .star_score');
+        const page = await browser.newPage();
+        await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36");
+        add_to_sheet(ws, 'C1', 's', '평점');
 
-                // 태그를 제대로 찾았으면
-                if(scoreEl) {
-                    // 그 내부의 텍스트 컨텐츠를 뱉어낸다.
-                    const text = await page.evaluate(tag => tag.textContent, scoreEl);
-                    console.log(record[0], '평점', text.trim());
+        // 탭을 새로 안 띄우고 한 페이지(page)에서 링크를 넘나들 것이다.
+        for(const [idx, record] of records.entries()) {
+            await page.goto(record.링크);
+            
+            // const 태그핸들러 = await page.$(선택자);
+            const text = await page.evaluate(() => {
+                // 이 내부에서는 document.querySelector 문법을 쓸 수 있다.
+                const score = document.querySelector('.score.score_left .star_score');
 
-                    // 결과들 배열화! 인덱스를 이용해 순서를 보장해주고 있다.
-                    result[idx] = [record[0], record[1], text.trim()];     
+                if(score) {
+                    return score.textContent;
                 }
-
-                // 페이지 로드 후 3초간 대기한다
-                await page.waitFor(3000);
-
-                // 한 페이지에 대해 크롤링 처리를 완료했으면 페이지를 닫아준다.
-                await page.close();
-            } catch(e) {
-                console.error(e);
+            });
+            
+            if(text) {
+                const newCell = 'C' + (idx + 2);
+                add_to_sheet(ws, newCell, 'n', parseFloat(text.trim()));
             }
-        }));
+
+            // 페이지 로드 후 3초간 대기한다
+            await page.waitFor(3000);
+        }
+            
+        // 한 페이지에 대해 크롤링 처리를 완료했으면 페이지를 닫아준다.
+        await page.close();
         
         // 브라우저 종료
         await browser.close();
 
-        // 결과 배열을 csv 파일에 다시 써 넣어준다.
-        const str = stringify(result);
-        fs.writeFileSync('./csv/result.csv', str, 'utf-8');
+        // 엑셀 파일에 써 넣기
+        xlsx.writeFile(workbook, './xlsx/result.xlsx')
     } catch(e) {
         console.error(e);
     }
