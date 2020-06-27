fcbc51592ae7ad637b515dffafadab618bfb090a
kyomin <kim031504@naver.com>
2020-06-27 22:57:41 +0900

    puppeteer와 csv를 이용한 네이버 영화 평점 크롤링 실습

--- index.js
+++ index.js
@@ -1,37 +1,48 @@
 /* module import */
-const axios = require('axios');
-const cheerio = require('cheerio');
-const xlsx = require('xlsx');
-const add_to_sheet = require('./add_to_sheet');
+const parse = require('csv-parse/lib/sync');
+const fs = require('fs');
+const puppeteer = require('puppeteer');
 
 /* file import */
-const workbook = xlsx.readFile('./xlsx/data.xlsx');
+const csv = fs.readFileSync('./csv/data.csv');
+const records = parse(csv.toString('utf-8'));
 
-// workbook.Sheets는 해당 엑셀 파일 내 시트 목록을 반환한다.
-// '영화목록'은 영화 정보를 저장한 시트 이름이다.
-// sheet_to_json를 통해 해당 시트의 정보들을 json 형태로 가공해 리스트로 반환한다.
-const ws = workbook.Sheets.영화목록;
-const records = xlsx.utils.sheet_to_json(ws);
-
+// 에러 처리 : 하나의 async 메소드 당 하나의 try ~ catch문을 넣어준다.
 const crawler = async () => {
-    add_to_sheet(ws, 'C1', 's', '평점');
-    for(const [i, record] of records.entries()) {
-        const response = await axios.get(record.링크);
-        if(response.status === 200) {       // 응답이 성공한 경우
-            const html = response.data;     // 클라이언트 view로 요청한 get은 html 응답이다.
-            const $ = cheerio.load(html);   // 이를 통해 html 태그들에 접근할 수 있다.
+    try {
+        const browser = await puppeteer.launch({ headless: process.env.NODE.ENV === 'production' });
+        
+        // Promise.all에 의해 각 페이지를 동시에 처리를 할 것이다.
+        await Promise.all(records.map(async (record, idx) => {
+            try {
+                // 페이지 객체 생성
+                const page = await browser.newPage();
+                
+                // 해당 링크의 페이지 띄우기
+                await page.goto(record[1]);
+                
+                // 해당 클래스로 특정하여 태그를 찾는다.
+                const scoreEl = await page.$('.score.score_left .star_score');
 
-            // 태그의 클래스로 선택을 특정한다.
-            // 해당 조건의 태그 내에서 text만 추출한다.
-            const text = $('.score.score_left .star_score').text();
-            console.log(record.제목, '평점', text.trim());
-            const newCell = 'C' + (i+2);        // 2 ~ 11까지
-            add_to_sheet(ws, newCell, 'n', parseFloat(text.trim()));
-        }
+                // 태그를 제대로 찾았으면
+                if(scoreEl) {
+                    // 그 내부의 텍스트 컨텐츠를 뱉어낸다.
+                    const text = await page.evaluate(tag => tag.textContent, scoreEl);
+                    console.log(record[0], '평점', text.trim());
+                }
+
+                // 한 페이지에 대해 크롤링 처리를 완료했으면 페이지를 닫아준다.
+                await page.close();
+            } catch(e) {
+                console.error(e);
+            }
+        }));
+        
+        // 브라우저 종료
+        await browser.close();
+    } catch(e) {
+        console.error(e);
     }
-
-    // 새롭게 result.xlsx 파일을 생성하며 결과를 써 넣는다.
-    xlsx.writeFile(workbook, './xlsx/result.xlsx');
 }
 
 crawler();
