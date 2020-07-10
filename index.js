78d40fbd04a83ee2dbab2ee9af00ff59c71bc5fd
kyomin <kim031504@naver.com>
2020-07-10 12:39:49 +0900

    puppeteer를 이용해 페이스북 로그인 작업 상호작용하기

--- index.js
+++ index.js
@@ -1,71 +1,34 @@
 /* module import */
 const puppeteer = require('puppeteer');
-const axios = require('axios');
-const fs = require('fs');
 
 
-/* folder handling */
-fs.readdir('imgs', (err) => {
-    if(err) {
-        console.error('imgs 폴더가 없어 imgs 폴더를 생성합니다.');
-        fs.mkdirSync('imgs');
-    }
-});
-
+/* customized module import */
+const dotenv = require('dotenv');
+dotenv.config();
 
 /* create crawler */
 const crawler = async () => {
     try {
-        const browser = await puppeteer.launch({ headless: false });
-        const page = await browser.newPage();
-        await page.goto('https://unsplash.com/');
-        let result = [];
-        
-        // 이미지는 30개까지 긁어오기
-        while(result.length <= 30) {
-            const srcs = await page.evaluate(() => {
-                window.scrollTo(0, 0);      // 초기 스크롤 맨 위로 옮기기
-                let imgs = [];
-                const imgEls = document.querySelectorAll('.nDTlD');
-                
-                if(imgEls.length) {
-                    imgEls.forEach((v) => {
-                        let src = v.querySelector('img._2zEKz');
-    
-                        if(src) {
-                            imgs.push(src.src);
-                        }
-    
-                        // 로딩된 이미지를 다운 받았으면 엘리먼트를 지운다.
-                        v.parentElement.removeChild(v);
-                    });
-                }
-    
-                // 자바스크립트를 이용해 스크롤 내리기. 세로로 500px 내리기.
-                // 그래야 지운 엘리먼트 자리에 새로운 이미지가 로딩된다.
-                window.scrollBy(0, 100);
-                setTimeout(() => {
-                    window.scrollBy(0, 500);
-                }, 500);
-    
-                return imgs;
-            });
-            result = result.concat(srcs);
-            await page.waitForSelector('.nDTlD');   // 해당 클래스의 태그가 로딩되길 기다린다.
-            console.log('새 이미지 태그 로딩 완료!');
-        }
-
-        // 이미지 소스들을 파일로 만들어서 imgs 폴더에 담기.
-        result.forEach(async (src, idx) => {
-            const imgResult = await axios.get(src.split('?')[0], {
-                responseType: 'arraybuffer'
-            });
-
-            fs.writeFileSync(`imgs/${new Date().valueOf()}.jpeg`, imgResult.data);
+        const browser = await puppeteer.launch({ 
+            headless: false, 
+            args: ['--window--size=1920, 1080'] 
         });
+        const page = await browser.newPage();
+        await page.setViewport({
+            width: 1920,
+            height: 1080
+        });
+        await page.goto('https://facebook.com/');
 
-        await page.close();
-        await browser.close();
+        const id = process.env.EMAIL;
+        const password = process.env.PASSWORD;
+        await page.evaluate((id, password) => {
+            document.querySelector('#email').value = id;
+            document.querySelector('#pass').value = password;
+            document.querySelector('#loginbutton').click();
+        }, id, password);
+        // await page.close();
+        // await browser.close();
     } catch(e) {
         console.error(e);
     }
