ef31cd43b01768922879d3ded0d65b1d6afb6880
kyomin <kim031504@naver.com>
2020-07-10 16:56:17 +0900

    페이스북 로그아웃 처리하기 완료

--- index.js
+++ index.js
@@ -32,6 +32,20 @@ const crawler = async () => {
 
         // 로그인 버튼을 클릭한다.
         await page.click('#loginbutton');
+
+        // 3초간 기다린다.
+        await page.waitFor(3000);
+
+        // 계정 팝업창 누르고
+        await page.waitForSelector('[aria-label="계정"]');
+        await page.click('[aria-label="계정"]');
+        
+        // 3초간 기다린후
+        await page.waitFor(3000);
+
+        // 로그아웃 처리
+        await page.waitForSelector('div.muag1w35.b20td4e0');
+        await page.click('.muag1w35.b20td4e0 div:nth-child(5) div');
     } catch(e) {
         console.error(e);
     }
