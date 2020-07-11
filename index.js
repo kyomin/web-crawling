2c54efbffddb0bcdb2105b96dea729db51bc6f5b
kyomin <kim031504@naver.com>
2020-07-11 13:48:30 +0900

    http://spys.one/en/free-proxy-list/ 사이트를 크롤링하여 프록시 ip 적용하기

--- index.js
+++ index.js
@@ -6,46 +6,58 @@ const puppeteer = require('puppeteer');
 const dotenv = require('dotenv');
 dotenv.config();
 
+
 /* create crawler */
 const crawler = async () => {
     try {
-        const browser = await puppeteer.launch({ 
+        let browser = await puppeteer.launch({ 
             headless: false, 
             args: ['--window--size=1920, 1080'] 
         });
-        const page = await browser.newPage();
+        let page = await browser.newPage();
         await page.setViewport({
             width: 1920,
             height: 1080
         });
-        await page.goto('https://facebook.com/');
-
-        // input 태그에 타이핑한다.
-        await page.type('#email', process.env.EMAIL);
-        await page.type('#pass', process.env.PASSWORD);
-
-        // 마우스를 로그인 버튼 위에 올린다.
-        await page.hover('#loginbutton');
-
-        // 3초간 기다린다.
-        await page.waitFor(3000);
-
-        // 로그인 버튼을 클릭한다.
-        await page.click('#loginbutton');
-
-        // 3초간 기다린다.
-        await page.waitFor(3000);
-
-        // 계정 팝업창 누르고
-        await page.waitForSelector('[aria-label="계정"]');
-        await page.click('[aria-label="계정"]');
+        await page.goto('http://spys.one/en/free-proxy-list/');
         
-        // 3초간 기다린후
-        await page.waitFor(3000);
+        const proxies = await page.evaluate(() => {
+            const ips = Array.from(document.querySelectorAll('tr > td:nth-child(1) > .spy14')).map((x) => {
+                let v = x.textContent
+                let ip = v.split(':')[0].split('d')[0];
+                let port = v.split(':')[2];
 
-        // 로그아웃 처리
-        await page.waitForSelector('div.muag1w35.b20td4e0');
-        await page.click('.muag1w35.b20td4e0 div:nth-child(5) div');
+                return ip + ':' + port;
+            });
+            const protocols = Array.from(document.querySelectorAll('tr > td:nth-child(2) > a > .spy1')).map((x) => x.textContent);
+            const latencies = Array.from(document.querySelectorAll('tr > td:nth-child(6) > .spy1')).map((x) => x.textContent);
+        
+            return ips.map((x, i) => {
+                return {
+                    ip: x,
+                    protocol: protocols[i],
+                    latency: latencies[i]
+                }
+            });
+        });
+
+        // HTTP 프로토콜을 사용하고 지연 시간이 작은 순서대로 정렬한다.
+        const filtered = proxies.filter((x) => x.protocol.startsWith("HTTP")).sort((a, b) => a.latency - b.latency);
+        
+        // 페이지와 브라우저를 닫고
+        await page.close();         
+        await browser.close();      
+
+        // 다시 브라우저를 킨다.
+        browser = await puppeteer.launch({ 
+            headless: false, 
+            args: ['--window--size=1920, 1080', `--proxy-server=${filtered[0].ip}`] 
+        });
+        page = await browser.newPage();
+        await page.setViewport({
+            width: 1920,
+            height: 1080
+        });
     } catch(e) {
         console.error(e);
     }
